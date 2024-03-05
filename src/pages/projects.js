677b46b4d972cc1aa533a8bd9cd53bd850ac1b5e
Pathiraja-D <pathirajadnew@gmail.com>
2024-03-05 08:43:56 +0530

    done

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -57,7 +57,7 @@ const projects = () => {
     </Head>
     <TransitionEffect/>
     <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
-        <Layout className='pt-16'>
+        <Layout className='pt-16 px-20 sm:px-0'>
             <AnimatedText text="Project Showcase" className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'/>
             <div className='grid grid-cols-12 gap-24 px-10 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
                 <div className='col-span-12 '>
